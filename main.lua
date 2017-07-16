ca179d6c24471238fd0c9ddd5f4d4d0508e00be0
murkrow92 <ducdt1992@gmail.com>
2017-07-16 16:02:01 +0700

    first commit

--- main.lua
+++ main.lua
@@ -5,4 +5,15 @@
 -- Time: 2:55 PM
 -- To change this template use File | Settings | File Templates.
 --
+a= 10
+b= 5
+c=4
 
+print(a+b+c)
+print(a-b-c)
+print(a*b*c)
+print(a/b/c)
+print( a > b and b >c)
+print(b<c)
+print(a>b or b<c)
+print( a-b> b-c)

d000a6576beedc5f631c38d348dd2c8aac9f52d7
murkrow <ducdt1992@gmail.com>
2017-07-16 20:14:10 +0700

    + add expo

--- main3.lua
+++ main3.lua
@@ -6,7 +6,16 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-function main()
-
+function gt(a)
+    if a == 1 then
+        return a
+    else
+        return a * gt(a - 1)
+    end
 end
 
+function main()
+    print(gt(10))
+end
+
+main()

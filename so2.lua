352515b1dbe521217e8455131cc5e3d63e44f304
murkrow92 <ducdt1992@gmail.com>
2017-07-16 16:01:34 +0700

    first commit

---
+++ so2.lua
@@ -0,0 +1,27 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/16/2017
+-- Time: 3:52 PM
+-- To change this template use File | Settings | File Template
+function printtypenumber(base)
+    if base > 0 then print("Duong")
+    else
+        print("Am")
+    end
+end
+
+function main()
+    local a = 10
+    local b = -324
+    local c = -242
+    local d = 3324
+    local e = 32
+    printtypenumber(a)
+    printtypenumber(b)
+    printtypenumber(c)
+    printtypenumber(d)
+    printtypenumber(e)
+end
+
+main()

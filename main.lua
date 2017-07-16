d605ef9f3f77d7401c388d4929a8f69ec222bbdd
murkrow <ducdt1992@gmail.com>
2017-07-16 15:52:07 +0700

    + function example

--- main.lua
+++ main.lua
@@ -4,5 +4,25 @@
 -- Date: 7/16/2017
 -- Time: 2:55 PM
 -- To change this template use File | Settings | File Templates.
---
 
+function printTypeNumber(base)
+    if base % 2 == 0 then
+        print("Chan")
+    else
+        print("Le")
+    end
+end
+
+function main()
+    local a = 10;
+    local b = 20;
+    local c = 30;
+    local d = 45;
+    printTypeNumber(a)
+    printTypeNumber(b)
+    printTypeNumber(c)
+    printTypeNumber(d)
+end
+
+
+main()

b5fcf9bdc8adbcb3f1b7c099168c75694784f8c7
murkrow <ducdt1992@gmail.com>
2017-07-16 16:38:09 +0700

    + add main3

---
+++ main3.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: murkrow
+-- Date: 7/16/17
+-- Time: 16:37
+-- To change this template use File | Settings | File Templates.
+--
+
+function main()
+
+end
+

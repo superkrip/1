5b5f0bdae4320e95b81347ad2fb1414e320e9269
murkrow92 <ducdt1992@gmail.com>
2017-07-20 12:36:34 +0700

    first commit

---
+++ baitap4.lua
@@ -0,0 +1,20 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/20/2017
+-- Time: 11:38 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+
+function sinx(x)
+    local a = math.sin(x)
+    return a
+end
+
+function main()
+    print(sinx(math.pi/6))
+end
+
+main()
+

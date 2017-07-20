ab1eb8d16795246b2fa43b4260a6c715aa6200cf
murkrow92 <ducdt1992@gmail.com>
2017-07-20 11:38:05 +0700

    first commit

---
+++ baitap3.lua
@@ -0,0 +1,21 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/20/2017
+-- Time: 11:32 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+function sosanh(a, b)
+    if a - b == 0 then print("a bang b") end
+    if a - b > 0 then print("a lon hon b") end
+    if a - b < 0 then print("a nho hon b") end
+
+
+end
+
+function main()
+print(sosanh(4, 4))
+end
+
+main()

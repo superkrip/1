97e9dc367ebea510b6b644f54d05222e62a64315
murkrow92 <ducdt1992@gmail.com>
2017-07-20 11:24:06 +0700

    first commit

---
+++ baitap1.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/20/2017
+-- Time: 11:14 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+function xxx(a, b)
+    local c = a * a + b * b
+    return c
+end
+
+function main()
+    print(xxx(4,4))
+end
+main()

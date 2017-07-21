a91ba9daa192d5a7620857030a4e1d5d4f63fb24
murkrow92 <ducdt1992@gmail.com>
2017-07-21 17:53:17 +0700

    first commit

---
+++ baitap6.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/21/2017
+-- Time: 5:40 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+function cosx(x)
+    return math.cos(x)
+end
+
+function main()
+    print(cosx(0.5))
+end
+
+main()

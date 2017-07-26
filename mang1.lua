b684db6fa6f8d9dd98e0efaf98d3e373847dabda
murkrow92 <ducdt1992@gmail.com>
2017-07-26 20:57:21 +0700

    first commit

---
+++ mang1.lua
@@ -0,0 +1,23 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/26/2017
+-- Time: 8:20 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+function max(x)
+    a = {}
+    for i = 1, i < n do
+        local b=0
+        if b < a[i] then b = a[i]
+        return b
+    end
+    end
+end
+
+function main()
+    print( { 1, 3, 4, 6, 7, 88, 9, 9, 9, 6, 4, 3, })
+end
+
+main()
